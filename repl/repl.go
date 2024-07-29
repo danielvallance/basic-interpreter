cb61130da7c0e729f97c45210fd26db742d9c4d2
danielvallance <danielvallance3435@gmail.com>
2024-07-29 21:16:50 +0100

    Integrated evaluator into the repl

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"basic-interpreter/evaluator"
 	"basic-interpreter/lexer"
 	"basic-interpreter/parser"
 	"bufio"
@@ -31,8 +32,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
